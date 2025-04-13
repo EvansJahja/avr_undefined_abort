53231814f7f156ef37589f0319d5dd20f75ed78d
Evans Jahja <evansjahja13@gmail.com>
2025-04-14 00:15:20 +0900

    Other case, without alloc

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,15 @@
 #![no_main]
 
 extern crate core;
-extern crate alloc;
 
-use core::{alloc::GlobalAlloc, panic::PanicInfo, str::FromStr};
-use alloc::string::String;
-
-
-struct Alloc;
-
-unsafe impl GlobalAlloc for Alloc {
-    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
-        todo!()
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
-        todo!()
-    }
-}
-
-#[global_allocator]
-static GLOBAL: Alloc = Alloc;
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
-
-pub static mut STATMUT : Option<String> = None;
-
 #[unsafe(no_mangle)]
 fn main() -> (){
-    unsafe {
-        STATMUT = Some(String::from_str("hello").unwrap());
-    }
-    loop {}
+    panic!("{}", 1);
 }
